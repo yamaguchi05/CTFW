8819a3191643dca9cdd2c25e293da79709b13ca6
vagrant <vagrant@localhost.localdomain>
2015-07-26 03:36:35 +0000

    vagrant構成ファイルをgitignoreに追加

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,8 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-knife[:berkshelf_path] = "cookbooks"
+log_level                :info
+log_location             STDOUT
+node_name                'vagrant'
+client_key               '/home/vagrant/CTFW/.chef/vagrant.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'localhost'
+syntax_check_cache_path  '/home/vagrant/CTFW/.chef/syntax_check_cache'
