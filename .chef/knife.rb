0bd7009967286d0fc10465de35d6b3819d4c501d
vagrant <vagrant@localhost.localdomain>
2015-07-26 03:42:22 +0000

    chef構成ファイルを削除、無視リストに追加

--- .chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'vagrant'
-client_key               '/home/vagrant/CTFW/.chef/vagrant.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'localhost'
-syntax_check_cache_path  '/home/vagrant/CTFW/.chef/syntax_check_cache'
